2af8aeffc306e3dbfc02277c9bf54bbc10301bff
baptistejouin <baptiste.jouin44@gmail.com>
2023-04-24 22:38:29 +0200

    feat: "/cookers" go get cookers list (to wip)

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-import mysql.connector as MC
+from flask import Flask, jsonify
+import mysql.connector
 
-try:
-	conn = MC.connect(host = 'localhost', database = 'imacook', user = 'root', password = "")
-except MC.Error as err:
-	print(err)
-finally:
-	if(conn.is_connected()):
-		conn.cursor.close()
-		conn.close()
-		conn.execute("SELECT * FROM cookers")
-		cookers = conn.fetchall()
-		conn.commit()
-		print(cookers)
+app = Flask(__name__)
+
+# define MySQL connection parameters
+db_config = {
+    'user': 'root',
+    'password': '',
+    'host': 'localhost',
+    'database': 'imacook'
+}
+
+# define route to get all cookers from the database
+@app.route('/cookers', methods=['GET'])
+def get_cookers():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM cookers")
+    rows = cursor.fetchall()
+    conn.close()
+
+    # convert rows to a list of dictionaries
+    cookers_list = []
+    for row in rows:
+        cooker = {
+            'id': row[0],
+            'name': row[1],
+            'password': row[2],
+        }
+        cookers_list.append(cooker)
+
+    # return the list of cookers as a JSON response
+    return jsonify(cookers_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
