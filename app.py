097391ba4dce19b2a79f1d9077bce86037383f1e
baptistejouin <baptiste.jouin44@gmail.com>
2023-05-21 09:58:40 +0200

    wip: api all recipes

--- app.py
+++ app.py
@@ -11,25 +11,37 @@ db_config = {
     'database': 'imacook'
 }
 
-# define route to get all cookers from the database
-@app.route('/cookers', methods=['GET'])
-def get_cookers():
+# define route to get all recipes from the database
+@app.route('/recipes', methods=['GET'])
+def get_recipes():
     conn = sqlite3.connect('database/imacook.db')
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM recipes")
-    rows = cursor.fetchall()
+    recipes_rows = cursor.fetchall()
+
+    # convert recipes_rows to a list of dictionaries
+    recipes_list = []
+    for recipe_row in recipes_rows:
+
+        cursor.execute("SELECT name FROM categories WHERE id = ? LIMIT 1", (recipe_row[1],))
+        category_rows = cursor.fetchall()
+
+
+        recipe = {
+            'id': recipe_row[0],
+            'name': recipe_row[4],
+            'category': category_rows[0][0],
+            # 'difficulty': recipe_row[2],
+            'cooker': recipe_row[3],
+            'cooking_time_s': recipe_row[6],
+            'picture': recipe_row[5],
+        }
+        recipes_list.append(recipe)
+
     conn.close()
 
-    # convert rows to a list of dictionaries
-    cookers_list = []
-    for row in rows:
-        cooker = {
-            'id': row[0],
-        }
-        cookers_list.append(cooker)
-
     # return the list of cookers as a JSON response
-    return jsonify(cookers_list)
+    return jsonify(recipes_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
