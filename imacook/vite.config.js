c80565ce910b42f3d3a1005d9951e482f9453c7b
baptistejouin <baptiste.jouin44@gmail.com>
2023-05-28 22:06:25 +0200

    fix env with vite

--- imacook/vite.config.js
+++ imacook/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  define: {
-    "process.env": {},
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    plugins: [vue()],
+    define: {
+      "process.env": env,
     },
-  },
-});
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  });
+};
